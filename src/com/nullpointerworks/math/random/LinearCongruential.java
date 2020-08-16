5df2b7b31711aab31f62761843d105cc2adce02c
Nullpointer Works <38044500+NullpointerWorks@users.noreply.github.com>
2020-08-16 11:51:19 +0200

    Update LinearCongruential.java
    fixed a 32-bit bug in the LCG class which operates at 64-bits.

--- src/com/nullpointerworks/math/random/LinearCongruential.java
+++ src/com/nullpointerworks/math/random/LinearCongruential.java
@@ -5,10 +5,8 @@
  */
 package com.nullpointerworks.math.random;
 
-import com.nullpointerworks.math.IntMath;
-
 /**
- * Linear Congruential Generator(a.k.a. LGC) is a seeded pseudo random number generator that follows a simple equation accompanied with a few magic numbers. There are various recipes available that produce varying results. Some produce very little randomization before repeating the series, others can produce millions of seemingly random numbers before any pattern appears.<br><br>
+ * Linear Congruential Generator(a.k.a. LCG) is a seeded pseudo random number generator that follows a simple equation accompanied with a few magic numbers. There are various recipes available that produce varying results. Some produce very little randomization before repeating the series, others can produce millions of seemingly random numbers before any pattern appears.<br><br>
  * The general equation for a LGC is:
  * <pre>x(i+1) = (a * x(i) + b) % m</pre>
  * where,
@@ -31,8 +29,14 @@ public class LinearCongruential extends AbstractRandomizer
 	 */
 	private final long a = 1664525;
 	private final long i = 1013904223;
-	private final long m = IntMath.pow(2,32);
+	private final long m = pow(2,32);
     private long x = 0;
+    private long pow(long n, long pow)
+	{
+		if (pow == 0) return 1;
+		if (pow == 1) return n;
+		return n * pow(n,pow-1);
+	}
     
     @Override
     public final void seed(long seed) 
@@ -46,7 +50,6 @@ public class LinearCongruential extends AbstractRandomizer
     	return x;
     }
     
-
     @Override
     public final double _double() 
     {
